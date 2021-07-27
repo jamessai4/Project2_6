11776dd370c83e193df8592837d0d2c3239433d6
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2021-07-27 12:47:37 +0700

    ..

--- loginpage/src/app/loginform/loginform.page.ts
+++ loginpage/src/app/loginform/loginform.page.ts
@@ -12,4 +12,22 @@ export class LoginformPage implements OnInit {
   ngOnInit() {
   }
 
+  showpass = false;
+  passtoggleicon = 'eye'
+
+  functioniconshowpass(): void{
+    this.showpass = !this.showpass;
+
+    // console.log(this.showpass)
+
+    if(this.passtoggleicon == 'eye'){
+      this.passtoggleicon = 'eye-off';
+    }
+    else{
+      this.passtoggleicon = 'eye';
+    }
+
+
+  }
+
 }
