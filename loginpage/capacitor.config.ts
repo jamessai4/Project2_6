b6ecaca20b3ed3c284b96055f1e7202b5045543d
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2021-07-27 10:46:01 +0700

    createProject

---
+++ loginpage/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'loginpage',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ loginpage/src/app/loginform/loginform.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginform',
+  templateUrl: './loginform.page.html',
+  styleUrls: ['./loginform.page.scss'],
+})
+export class LoginformPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
